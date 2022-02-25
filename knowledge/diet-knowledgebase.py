1f62e1abce4d52056bb97514951618e88a2b473d
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2022-02-25 17:00:34 +0300

    add dish finding query
    Took 1 hour 12 minutes

--- knowledge/diet-knowledgebase.py
+++ knowledge/diet-knowledgebase.py
@@ -2,17 +2,18 @@ from pyswip import Prolog
 
 if __name__ == '__main__':
     prolog = Prolog()
-    prolog.assertz("содержит(X,[X|_]).")
-    prolog.assertz("содержит(X, [_|Hv]) :- содержит(X,Hv).")
+    prolog.assertz("member(X, [X | _])")
+    prolog.assertz("member(X, [_ | Hv]) :- member(X, Hv)")
+    prolog.assertz("common_elements([H | _], L2) :- member(H, L2)")
+    prolog.assertz("common_elements([_ | T], L2) :- common_elements(T, L2)")
 
-    prolog.assertz("симптомы(кандида,антикандида).")
+    prolog.assertz("symptoms(кандида,антикандида)")
 
-    prolog.assertz("диета(антикандида,[говядина,телятина,молоко,рис],[птица,сельд,яйца,бобы,гречка]).")
+    prolog.assertz("diet(антикандида,[говядина,телятина,молоко,рис],[птица,сельд,яйца,бобы,гречка])")
 
-    prolog.assertz("блюдо(йогурт,[молоко,клубника,закваска]).")
-    prolog.assertz("блюдо(зразы из индейки, [курица, лук,соль,яйцо,укроп,зеленый салат,сливочное масло]).")
-    # list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
-    for soln in prolog.query("father(X,Y)"):
-        print(soln["X"], "is the father of", soln["Y"])
-    # michael is the father of john
-    # michael is the father of gina
+    prolog.assertz("dish(йогурт,[молоко,клубника,закваска])")
+    prolog.assertz("dish(\"зразы из индейки\", [птица, лук, соль, яйцо, укроп, \"зеленый салат\", \"сливочное масло\"])")
+
+    for solution in prolog.query("dish(X,Y),symptoms(кандида, D),diet(D,_,A),common_elements(A,Y)."):
+        print("Diet for candida is ", solution["D"], ", dish is ", solution["X"])
+
