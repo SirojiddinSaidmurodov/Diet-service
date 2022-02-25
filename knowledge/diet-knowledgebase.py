1b7235b106a17d261fa50aea3e71d4662f8c206c
SITihomirov <sirtikhomirov@gmail.com>
2022-02-26 01:54:31 +0300

    update knowledge base

--- knowledge/diet-knowledgebase.py
+++ knowledge/diet-knowledgebase.py
@@ -8,11 +8,18 @@ if __name__ == '__main__':
     prolog.assertz("common_elements([_ | T], L2) :- common_elements(T, L2)")
 
     prolog.assertz("symptoms(кандида,антикандида)")
+    prolog.assertz("symptoms(\"плохое усвоение углеводов\",\"low fodmap\")")
+    prolog.assertz("symptoms(\"пищевая зависимость\",lchf)")
 
     prolog.assertz("diet(антикандида,[говядина,телятина,молоко,рис],[птица,сельд,яйца,бобы,гречка])")
+    prolog.assertz("diet(\"low fodmap\",[\"манная крупа\",хлеб,молоко],[гречка,рис,\"молоко без лактозы\"])")
+    prolog.assertz("diet(lchf,[молоко,шоколад,консервы,чеснок,лук],[сало,птица,немолоко,апельсин,шпинат,корнишоны])")
 
     prolog.assertz("dish(йогурт,[молоко,клубника,закваска])")
     prolog.assertz("dish(\"зразы из индейки\", [птица, лук, соль, яйцо, укроп, \"зеленый салат\", \"сливочное масло\"])")
+    prolog.assertz("dish(\"кальмаровый салат\", [кальмар, огурец, соль, яйцо, перец, майонез])")
+    prolog.assertz("dish(\"судак с подушкой из шпината\", [судак,\"лимонный сок\", \"оливковое масло\", "
+                   "соль, лук, шпинат,\"сливочное масло\"])")
 
     for solution in prolog.query("dish(X,Y),symptoms(кандида, D),diet(D,_,A),common_elements(A,Y)."):
         print("Diet for candida is ", solution["D"], ", dish is ", solution["X"])
