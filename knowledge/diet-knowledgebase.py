06b05f7b18d19c2d4bdee8332a7948b8bc684616
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2022-03-04 14:13:50 +0300

    add product quantity + rule for getting products from nested lists
    Took 38 minutes

--- knowledge/diet-knowledgebase.py
+++ knowledge/diet-knowledgebase.py
@@ -4,5 +4,6 @@ if __name__ == '__main__':
     prolog = Prolog()
     prolog.consult("diet.pl")
 
-    for solution in prolog.query("dish(X,Y), symptoms(\"пищевая зависимость\", D), diet(D,_,A), common_elements(A,Y)."):
+    for solution in prolog.query(
+            "dish(X,Y),dishProducts(Y,P), symptoms(\"пищевая зависимость\", D), diet(D,_,A), common_elements(A,P)."):
         print("Diet for candida is ", solution["D"], ", dish is ", solution["X"])
