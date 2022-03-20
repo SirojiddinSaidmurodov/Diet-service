7e5b0e6b2e3eb98c91de72aa598806ccd58b0dc4
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2022-03-20 11:23:12 +0300

    fix encoding
    Took 1 hour 24 minutes

--- knowledge/diet-knowledgebase.py
+++ knowledge/diet-knowledgebase.py
@@ -2,8 +2,16 @@ from pyswip import Prolog
 
 if __name__ == '__main__':
     prolog = Prolog()
-    prolog.consult("diet.pl")
+    prolog.consult("diet.pl", catcherrors=True)
+
+
+    def encode(result: dict):
+        for key in result.keys():
+            if type(result.get(key)) == bytes:
+                result[key] = result.get(key).decode('utf-8')
+
 
     for solution in prolog.query(
             "dish(X,Y),dishProducts(Y,P), symptoms(\"пищевая зависимость\", D), diet(D,_,A), common_elements(A,P)."):
+        encode(solution)
         print("Diet for candida is ", solution["D"], ", dish is ", solution["X"])
