46e8f3a44b6be7330adbbce7905475f6da1ef133
SITihomirov <sirtikhomirov@gmail.com>
2022-03-29 10:13:38 +0300

    update base and add urls for api

--- diet_service_backend/urls.py
+++ diet_service_backend/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
-from diet_service import views
+
+from diet_service_backend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/get-diet', views.diet)
+    path('api/get-diet/', views.diet),
 ]
